c295ecbaf5bf497484afddae1a90f7c4cf4027ff
magoreal4 <magoreal4@gmail.com>
2020-09-30 11:03:12 -0400

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,23 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:pozos8/provider/firebase.dart';
-import 'package:pozos8/utils/dateFormat.dart';
 import 'package:provider/provider.dart';
 
+import 'package:pozos8/provider/firebase.dart';
 import 'package:pozos8/pages/distribudor_page.dart';
 import 'package:pozos8/pages/newreg_page.dart';
 import 'package:pozos8/pages/firstlog_page.dart';
 import 'package:pozos8/utils/sharedP.dart';
 
-Future<void> getPermission() async {
-  // Verificar el estado de permisos y conexión
-  LocationPermission permission = await checkPermission();
-  if (permission != LocationPermission.always) {
-    LocationPermission permission = await requestPermission();
-    if (permission != LocationPermission.always) {
-      await openLocationSettings();
-    }
-  }
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterBackgroundService.initialize(onStart);
   final prefs = new SharedP();
   await prefs.initPrefs();
-  await Firebase.initializeApp();
-  await getPermission();
-
-  // Inicia el servicio en un isolate aparte
-  FlutterBackgroundService.initialize(onStart);
+  // await Firebase.initializeApp();
 
   runApp(ChangeNotifierProvider(
     create: (context) => SharedP(),
@@ -42,135 +25,22 @@ void main() async {
   ));
 }
 
-final prefs = new SharedP();
-enum LocationStatus { UNKNOWN, RUNNING, STOPPED }
-CollectionReference pos = FirebaseFirestore.instance.collection('tracking');
-Stream<DocumentSnapshot> _request =
-    FirebaseFirestore.instance.collection('All').doc('position').snapshots();
-
-// Parametros para el posicionamiento continuo
-Stream<Position> positionStream = getPositionStream(
-    desiredAccuracy: LocationAccuracy.best,
-    distanceFilter: 25,
-    timeInterval: 120000); // cada 120 segundos
-StreamSubscription<Position> positionStreamS;
-LocationStatus _status = LocationStatus.UNKNOWN;
+// enum LocationStatus { UNKNOWN, RUNNING, STOPPED }
+// LocationStatus _status = LocationStatus.UNKNOWN;
 
 // --------Servicios en Background--------
 void onStart() async {
   // Todos lo sparametros para se ejecuten en el otro isolate
   WidgetsFlutterBinding.ensureInitialized();
+  print("Iniciando en Background");
   final service = FlutterBackgroundService();
-  await Firebase.initializeApp();
   final prefs2 = new SharedP();
   await prefs2.initPrefs();
-
-  // Escucha cambios para todos los usuarios
-  _request.listen(onData);
-  positionStreamS = positionStream.listen(contPosition);
-
-  // _contPos.listen((event) {
-  //   if (event['contPosition']) {
-  //     positionStreamS = positionStream.listen(contPosition);
-  //   } else {
-  //     inicial ? inicial = false : positionStreamS.cancel();
-  //   }
-  // });
 
   service.setNotificationInfo(
     title: "Servicio Pozo Séptico",
     content: "Hola",
   );
-
-  // Position position0 = await getLastKnownPosition();
-
-  // final service = FlutterBackgroundService();
-  // final audioPlayer = AudioPlayer();
-
-  // String url =
-  //     "https://www.mediacollege.com/downloads/sound-effects/nature/forest/rainforest-ambient.mp3";
-
-  // audioPlayer.onPlayerStateChanged.listen((event) {
-  //   if (event == AudioPlayerState.COMPLETED) {
-  //     audioPlayer.play(url);
-  //   }
-  // });
-
-  // audioPlayer.play(url);
-
-  // service.onDataReceived.listen((event) {
-  //   print(event);
-  // });
-
-  // Timer.periodic(Duration(seconds: 1), (timer) {
-  //   service.setNotificationInfo(
-  //     title: "My App Service",
-  //     content: "Updated at ${DateTime.now()}",
-  //   );
-
-  //   service.sendData(
-  //     {"current_date": DateTime.now().toIso8601String()},
-  //   );
-  // });
-}
-
-// Funcion de background oara la localización continua
-void contPosition(Position locationDto) async {
-  final service = FlutterBackgroundService();
-  // Todo lo que es prefs2 es porque esta en el background
-  final prefs2 = new SharedP();
-
-  if (_status == LocationStatus.UNKNOWN) {
-    _status = LocationStatus.RUNNING;
-  }
-  Map<String, dynamic> _time0;
-  Map<String, dynamic> _time1 = locationDto.toJson();
-  (prefs2.location == '')
-      ? _time0 = _time1
-      : _time0 = json.decode(prefs2.location);
-  prefs2.location = json.encode(_time1);
-  final int difference =
-      ((_time1['timestamp'] - _time0['timestamp']) / 1000).round();
-
-  print('Segundos $difference');
-
-  (difference > 300) // si se queda mas de xxx segundos
-      ? _time0.addAll({'estadia': difference.toString(), 'title': 'stop'})
-      : _time0.addAll({'estadia': '0', 'title': 'track'});
-
-  // Envia al hilo principal para ser enviado a firebase
-  service.sendData(_time0);
-}
-
-// Escucha en segundo plano para enviar la posicion instantanea
-void onData(DocumentSnapshot data) async {
-  // if (data.data()['location'] ?? false) {
-  print("Request location: ${data.data()}");
-  Position _position =
-      await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
-  var _date = formatDate(dateTime: DateTime.now());
-
-  Map<String, dynamic> repFields = {
-    "lat": _position.latitude,
-    "lon": _position.longitude,
-    "speed": _position.speed.round(),
-    "heading": _position.heading.round()
-  };
-  Map<String, dynamic> rep = {
-    "title": "track",
-    "date": _date,
-    "status": "publish",
-    "author": prefs.userID,
-    "fields": repFields
-  };
-
-  await pos
-      .doc(_date)
-      .set(rep)
-      .then((value) => print("Position Encontrada"))
-      .catchError((error) => print("No se pudo encontrar la posición: $error"));
-  // }
 }
 
 class MyApp extends StatefulWidget {
@@ -184,9 +54,20 @@ class _MyAppState extends State<MyApp> {
   Stream contPos = FlutterBackgroundService().onDataReceived;
   StreamSubscription contPosSubscription;
 
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+    } catch (e) {
+      print("Ocurrio un error: $e");
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    initializeFlutterFire();
+
     // Recibe los datos de la posición contínua del background
     contPosSubscription = contPos.listen(onData);
   }
@@ -219,7 +100,6 @@ class _MyAppState extends State<MyApp> {
       print('-------------------------------------------------');
       print('$rep');
       prefs.pos = json.encode(rep);
-      print(rep);
 
       prefs.contPos ? await FirebaseProvider.nuevoTracking(reporte: rep) : null;
     } else {
--- lib/pages/distribudor_page.dart
+++ lib/pages/distribudor_page.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:pozos8/pages/home_page.dart';
 import 'package:pozos8/pages/newreg_page.dart';
 import 'package:pozos8/pages/settings_page.dart';
+import 'package:pozos8/utils/dateFormat.dart';
 import 'package:pozos8/utils/sharedP.dart';
 
 class DistribuidorPage extends StatefulWidget {
@@ -20,31 +24,112 @@ class DistribuidorPage extends StatefulWidget {
 class _DistribuidorPageState extends State<DistribuidorPage> {
   int currenIndex = 0;
   final prefs = new SharedP();
-  StreamSubscription contPosFireSubscription;
+  // StreamSubscription contPosFireSubscription;
+
+  // Stream<DocumentSnapshot> _request =
+  //     FirebaseFirestore.instance.collection('All').doc('position').snapshots();
+
+  // CollectionReference pos = FirebaseFirestore.instance.collection('tracking');
+
+  // // Parametros para el posicionamiento continuo
+  // Stream<Position> positionStream = getPositionStream(
+  //     desiredAccuracy: LocationAccuracy.best,
+  //     distanceFilter: 25,
+  //     timeInterval: 120000); // cada 120 segundos
+  // StreamSubscription<Position> positionStreamS;
 
   @override
   void initState() {
     super.initState();
+    getPermission();
+    // Inicia el servicio en un isolate aparte
+
     // Escucha cambios de firestore para enviarlo al background
-    Stream<DocumentSnapshot> contPosFire = FirebaseFirestore.instance
-        .collection('choferes')
-        .doc(prefs.nameUser)
-        .snapshots();
-    contPosFireSubscription = contPosFire.listen(contPostData);
+    // Stream<DocumentSnapshot> contPosFire = FirebaseFirestore.instance
+    //     .collection('choferes')
+    //     .doc(prefs.nameUser)
+    //     .snapshots();
+    // contPosFireSubscription = contPosFire.listen(contPostData);
+
+    // // Escucha cambios para todos los usuarios
+    // _request.listen(onData);
+    // positionStreamS = positionStream.listen(contPosition);
   }
 
   @override
   void dispose() {
     super.dispose();
-    contPosFireSubscription.cancel();
+
+    // contPosFireSubscription.cancel();
+    // positionStreamS.cancel();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: callPage(currenIndex),
-      bottomNavigationBar: _bottomNavigationBar(context),
-    );
+  Future<void> getPermission() async {
+    // Verificar el estado de permisos y conexión
+    LocationPermission permission = await checkPermission();
+    if (permission != LocationPermission.always) {
+      LocationPermission permission = await requestPermission();
+      if (permission != LocationPermission.always) {
+        await openLocationSettings();
+      }
+    }
+  }
+
+  void onData(DocumentSnapshot data) async {
+    print("Request location: ${data.data()}");
+    Position _position =
+        await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    var _date = formatDate(dateTime: DateTime.now());
+
+    Map<String, dynamic> repFields = {
+      "lat": _position.latitude,
+      "lon": _position.longitude,
+      "speed": _position.speed.round(),
+      "heading": _position.heading.round()
+    };
+    Map<String, dynamic> rep = {
+      "title": "track",
+      "date": _date,
+      "status": "publish",
+      "author": prefs.userID,
+      "fields": repFields
+    };
+
+    // await pos
+    //     .doc(_date)
+    //     .set(rep)
+    //     .then((value) => print("Position Encontrada"))
+    //     .catchError(
+    //         (error) => print("No se pudo encontrar la posición: $error"));
+  }
+
+// Funcion de background oara la localización continua
+  void contPosition(Position locationDto) async {
+    final service = FlutterBackgroundService();
+    // Todo lo que es prefs2 es porque esta en el background
+    final prefs2 = new SharedP();
+
+    // if (_status == LocationStatus.UNKNOWN) {
+    //   _status = LocationStatus.RUNNING;
+    // }
+    Map<String, dynamic> _time0;
+    Map<String, dynamic> _time1 = locationDto.toJson();
+    (prefs2.location == '')
+        ? _time0 = _time1
+        : _time0 = json.decode(prefs2.location);
+    prefs2.location = json.encode(_time1);
+    final int difference =
+        ((_time1['timestamp'] - _time0['timestamp']) / 1000).round();
+
+    print('Segundos $difference');
+
+    (difference > 300) // si se queda mas de xxx segundos
+        ? _time0.addAll({'estadia': difference.toString(), 'title': 'stop'})
+        : _time0.addAll({'estadia': '0', 'title': 'track'});
+
+    // Envia al hilo principal para ser enviado a firebase
+    service.sendData(_time0);
   }
 
   // Escucha cambios de firestore para enviarlo al backcround
@@ -54,6 +139,14 @@ class _DistribuidorPageState extends State<DistribuidorPage> {
     FlutterBackgroundService().sendData({
       "contPosition": prefs.contPos,
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: callPage(currenIndex),
+      bottomNavigationBar: _bottomNavigationBar(context),
+    );
   }
 
   Widget _bottomNavigationBar(BuildContext context) {
